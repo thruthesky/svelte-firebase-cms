d66836f94bf620beee3697a1f2ecc3b49986882d
Noe <withcenter.dev4@gmail.com>
2024-04-25 16:43:40 +0800

    remove my database use test db

--- src/init.firebase.client.ts
+++ src/init.firebase.client.ts
@@ -1,13 +1,5 @@
 import { initializeApp } from "firebase/app";
-const firebaseConfig: { [key: string]: string; } = import.meta.env.MODE == 'dev4' ? {
-    apiKey: "AIzaSyBBtKOZkwQtXWu7QrjcLHYMg23sOvj6mdU",
-    authDomain: "app-test-db-357d4.firebaseapp.com",
-    projectId: "app-test-db-357d4",
-    storageBucket: "app-test-db-357d4.appspot.com",
-    messagingSenderId: "47750314707",
-    appId: "1:47750314707:web:936d4838bbdb78cbe27226",
-    measurementId: "G-QFWSWH12W4"
-  } : {
+const firebaseConfig: { [key: string]: string; } ={
     apiKey: "AIzaSyA2q3ux4nqf2-iWY6nPTqqCRvqHQ70aWfU",
     authDomain: "silbus.firebaseapp.com",
     databaseURL: "https://silbus-default-rtdb.asia-southeast1.firebasedatabase.app",

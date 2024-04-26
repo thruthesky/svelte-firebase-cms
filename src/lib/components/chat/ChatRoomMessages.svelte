adfd392360d63499435ce4f6bae02d2d274e9206
Christian Bucal <withcenter-dev2@github.com>
2024-04-26 16:31:11 +0800

    fix import path

--- src/lib/components/chat/ChatRoomMessages.svelte
+++ src/lib/components/chat/ChatRoomMessages.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount, tick } from 'svelte';
-	import InfiniteScroll from './InfiniteScroll.svelte';
+	import InfiniteScroll from './ChatMessageInfiniteScrollTrigger.svelte';
 	export let chatRoomId: string;
 
 	let _messages: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
